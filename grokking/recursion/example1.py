6149b8d4be484befae68e0a6402b43b96280f7d0
julienroyd <jul.roy1311@gmail.com>
2022-01-24 17:11:28 -0500

    Added small example exposing the stack when using recursion

---
+++ grokking/recursion/example1.py
@@ -0,0 +1,19 @@
+import traceback
+
+def print_stack():
+    print('\n\n----------------------------')
+    for line in traceback.format_stack():
+        print(line.strip())
+    print('----------------------------')
+
+def print_backward(x):
+    print_stack()
+    if x == 0:
+        print(f"Reached x={x}")
+    else:
+        print(f"Printing x={x}")
+        print_backward(x - 1)
+        print(f"Ended with call x={x}")
+
+if __name__ == '__main__':
+    print_backward(3)
