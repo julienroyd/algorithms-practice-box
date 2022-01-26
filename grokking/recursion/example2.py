64b2dddb69f671f2bd9854e3b676ae719c9143bf
julienroyd <jul.roy1311@gmail.com>
2022-01-25 20:28:39 -0500

    Added simple examples of recursion

--- grokking/recursion/example2.py
+++ grokking/recursion/example2.py
@@ -6,15 +6,29 @@ def traditional_sum(array):
         total += x
     return total
 
-
 def recursive_sum(array):
     if len(array) == 0:
         return 0
     else:
         return array[0] + recursive_sum(array[1:])
 
+def recursive_count(array):
+    if len(array) == 0:
+        return 0
+    else:
+        return 1 + recursive_count(array[1:])
+
+def recursive_max(array):
+    if len(array) == 2:
+        return array[0] if array[0] > array[1] else array[1]
+    else:
+        tmp_max = recursive_max(array[1:])
+        return array[0] if array[0] > tmp_max else tmp_max
+
 
 if __name__ == '__main__':
-    array = [1, 2, 3, 4, 5, 6, 7, 8]
+    array = [1, 2, 3, 4, 5, 60, 7, 8]
     print(f"total={traditional_sum(array)}")
     print(f"total={recursive_sum(array)}")
+    print(f"count={recursive_count(array)}")
+    print(f"max={recursive_max(array)}")
